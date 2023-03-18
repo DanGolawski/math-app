2849fd4f13250373ff3cecb0f3f50b7fed9ed271
User <user@pc-1888.home>
2023-03-18 13:49:46 +0100

    change backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   youtubeUrl: 'https://www.googleapis.com/youtube/v3',
   apiKey: 'AIzaSyB28Q2vsOYPohxrhgVbwkQR4OS3nQLetEo',
   channelId: 'UCpRSDLbsLxYf-wYDycO597Q',
-  mathAppApi: 'http://localhost:3000'
+  mathAppApi: 'https://math-app-backend-ybsr.onrender.com'
 };
 
 /*
